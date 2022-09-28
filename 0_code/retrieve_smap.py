065daf43d83eb4fa91ff057d743fe22c73e71869
Ryoko Araki <400mhs2@gmail.com>
2022-09-27 20:26:04 -0700

    Reorganized + added sleep time to check updates

--- 0_code/retrieve_smap.py
+++ 0_code/retrieve_smap.py
@@ -1,32 +1,13 @@
-# This script samples SMAP data using point information (geographic coordinates)
+# ======================= THE SCRIPT ===============================
+## This script samples SMAP data using point information (geographic coordinates)
 # The Appears' API documentation is here: https://appeears.earthdatacloud.nasa.gov/api/?python#introduction
 # The Appears' website is here: https://appeears.earthdatacloud.nasa.gov/
 
-import json
-import requests
-import os
-
-# Specify current directory create output directory if it does not exist
-os.chdir("G:/Shared drives/Ryoko and Hilary/SMSigxSMAP/analysis/0_code")
-
-## Some parameters
-params = {'pretty': True}
-
-
-# The data I am looking at is SMAP Enhanced L3 Radiometer Global and Polar Grid Daily 9 km EASE-Grid Soil Moisture, Version 5 (SPL3SMP_E)
+# ======================= THE DATA ===============================
+# This script downloads SMAP Enhanced L3 Radiometer Global and Polar Grid Daily 9 km EASE-Grid Soil Moisture, Version 5 (SPL3SMP_E)
 # https://nsidc.org/data/spl3smp_e/versions/5#anchor-2
 
-# A list of product code is here
-# https://appeears.earthdatacloud.nasa.gov/products
-
-## Request a sample
-product_id = 'SPL3SMP_E.005'
-response = requests.get(
-    'https://appeears.earthdatacloud.nasa.gov/api/product/{0}'.format(product_id),
-    params=params)
-product_response = response.text
-print(product_response)
-
+# Layers downloaded are as follows:
 # Soil_Moisture_Retrieval_Data_AM_retrieval_qual_flag (have different versions)
 # Soil_Moisture_Retrieval_Data_AM_soil_moisture
 # Soil_Moisture_Retrieval_Data_AM_soil_moisture_error
@@ -34,11 +15,43 @@ print(product_response)
 # Soil_Moisture_Retrieval_Data_PM_soil_moisture_pm
 # Soil_Moisture_Retrieval_Data_PM_soil_moisture_error_pm
 
+# A list of product code is here
+# https://appeears.earthdatacloud.nasa.gov/products
+
+# ======================= START OF THE CODE ===============================
+
+# Import libraries
+import json
+import requests
+import os
+from ismn.interface import ISMN_Interface
+
+# Specify current directory and create output directory if it does not exist
+os.chdir("G:/Shared drives/Ryoko and Hilary/SMSigxSMAP/analysis/0_code")
+
 ## Prepare tasks
-# load the task request from a file
+
+# Parameters
+params = {'pretty': True}
+
+# Get the target point locations (ISMN sensor coordinates)
+network_name = 'OZNET'
+in_path = 'G:/Shared drives/Ryoko and Hilary/SMSigxISMN/analysis/0_data_raw/ISMN'
+data_net = ISMN_Interface(in_path, network=[network_name])
+lat_ismn=[]
+lon_ismn=[]
+for network, station, sensor in data_net.collection.iter_sensors(variable='soil_moisture'): # Only select the surface soil moisture within 0-5cm sensor depth to compare with SMAP data
+    # Read metadata
+    meta_pd = sensor.metadata.to_pd()
+    lat_ismn.append(meta_pd['latitude'].values[0])
+    lon_ismn.append(meta_pd['longitude'].values[0])
+
+# Load the task request template from a file
 sample_request_path = "./sample_point_request.json"
 with open(sample_request_path) as json_file:
     task = json.load(json_file)
+
+# Write a json for target point locations
 
 ## Login to appears
 my_credential_path = "./auth.json"
@@ -49,7 +62,7 @@ response = requests.post('https://appeears.earthdatacloud.nasa.gov/api/login', a
 token_response = response.json()
 print(token_response)
 
-# submit the task request
+# Submit the task request
 token = token_response['token']
 response = requests.post(
     'https://appeears.earthdatacloud.nasa.gov/api/task',
@@ -58,15 +71,19 @@ response = requests.post(
 task_response = response.json()
 print(task_response)
 
-# To check the updates
+# To check the updates every x seconds
 task_id = task_response['task_id']
-response = requests.get(
-    'https://appeears.earthdatacloud.nasa.gov/api/task/{0}'.format(task_id),
-    headers={'Authorization': 'Bearer {0}'.format(token)}
-)
-task_response = response.json()
-print(task_response)
-print(task_response['status'])
+
+import time
+starttime = time.time()
+while task_response['status']=='pending' or task_response['status']=='processing':
+    print("Still processing the request at %s" % {time.time()})
+    response = requests.get(
+        'https://appeears.earthdatacloud.nasa.gov/api/task/{0}'.format(task_id),
+        headers={'Authorization': 'Bearer {0}'.format(token)}
+    )
+    task_response = response.json()
+    time.sleep(60.0 - ((time.time() - starttime) % 60.0))
 
 # To bundle download
 response = requests.get(
@@ -81,7 +98,7 @@ for i in range(len(bundle_response)):
 
     # get a stream to the bundle file
     file_id = bundle_response['files'][i]['file_id']
-    filename = file_id
+    filename = bundle_response['files'][i]['file_name']
     response = requests.get(
         'https://appeears.earthdatacloud.nasa.gov/api/bundle/{0}/{1}'.format(task_id,file_id),
         headers={'Authorization': 'Bearer {0}'.format(token)},
@@ -90,7 +107,7 @@ for i in range(len(bundle_response)):
     )
 
     # create a destination directory to store the file in
-    dest_dir = "../1_data"
+    dest_dir = "../1_data/SMAP"
     filepath = os.path.join(dest_dir, filename)
     os.makedirs(os.path.dirname(filepath), exist_ok=True)
 
@@ -99,5 +116,18 @@ for i in range(len(bundle_response)):
         for data in response.iter_content(chunk_size=8192):
             f.write(data)
 
+
+# ======================= END OF THE CODE ===============================
+
 # Point sampling returns the results as csv file
 # Area sampling returns the results as nc file
+
+"""
+## Request a sample
+product_id = 'SPL3SMP_E.005'
+response = requests.get(
+    'https://appeears.earthdatacloud.nasa.gov/api/product/{0}'.format(product_id),
+    params=params)
+product_response = response.text
+print(product_response)
+"""
