aca77ac71047fd063a0f10725c10789c2d14e0a5
RY4GIT <400mhs2@gmail.com>
2024-03-17 12:18:00 -0700

    readjust method fig

--- notebooks/figures_for_manuscript_method.py
+++ notebooks/figures_for_manuscript_method.py
@@ -8,38 +8,6 @@ import ast
 
 from functions import q_drydown, exponential_drydown, loss_model, exponential_drydown2
 
-# %%
-# Define plot format
-var_dict = {
-    "theta": {
-        "column_name": "sm",
-        "symbol": r"$\theta$",
-        # "label": r"Soil moisture",
-        "label": r"Soil moisture, $\theta$",
-        "unit": r"(m$^3$ m$^{-3}$)",
-        "lim": [0, 0.50],
-    },
-    "dtheta": {
-        "column_name": "",
-        "symbol": r"$-\frac{d\theta}{dt}$",
-        "label": r"$\minus\frac{d\theta}{dt}$",
-        "unit": r"(m$^3$ m$^{-3}$ day$^{-1}$)",
-        "lim": [-0.10, 0],
-    },
-    "t": {
-        "column_name": "",
-        "symbol": r"$t$",
-        "label": r"$t$",
-        "unit": r"(days)",
-    },
-}
-theta_vardict = var_dict["theta"]
-dtheta_vardict = var_dict["dtheta"]
-
-plt.rcParams["mathtext.fontset"] = (
-    "stixsans"  #'stix'  # Or 'cm' (Computer Modern), 'stixsans', etc.
-)
-
 
 # %%
 # Define parameters
@@ -52,30 +20,67 @@ Global q>1 median: 2.47
 Global q<1 mean: 4.03
 """
 q0 = 1
-q1 = 2.47
-q2 = 0.59
-
+q1 = 1.5
+q2 = 0.7
+# q1 = 2.47
+# q2 = 0.59
 
 # Common parameters
 k = 0.3
+# k = 0.1
 theta_w = 0.02
-theta_star = 0.5
+theta_star = 0.6
 delta_theta = theta_star - theta_w
 fc_minus_star = 0.05
 theta_fc = theta_star + fc_minus_star
+
 # Define variables
 theta = np.arange(theta_w, theta_star, 1e-03)
 theta_under_wp = np.arange(0, theta_w, 1e-03)
 theta_above_star = np.arange(theta_star, theta_star + fc_minus_star, 1e-03)
 
-t = np.arange(0, 10, 1e-03)
+tmax = 10
+t = np.arange(0, tmax, 1e-03)
+
+# %%
+# Define plot format
+var_dict = {
+    "theta": {
+        "column_name": "sm",
+        "symbol": r"$\theta$",
+        # "label": r"Soil moisture",
+        "label": r"Soil moisture, $\theta$",
+        "unit": r"($m^3$ $m^{-3}$)",
+        "lim": [0, theta_fc],
+    },
+    "dtheta": {
+        "column_name": "",
+        "symbol": r"$-\frac{d\theta}{dt}$",
+        "label": r"$\minus\frac{d\theta}{dt}$",
+        "unit": r"($m^3$ $m^{-3}$ $day^{-1}$)",
+        "lim": [-0.08, 0],
+    },
+    "t": {
+        "column_name": "",
+        "symbol": r"$t$",
+        "label": r"$t$",
+        "unit": r"($day$)",
+    },
+}
+theta_vardict = var_dict["theta"]
+dtheta_vardict = var_dict["dtheta"]
+
+plt.rcParams["mathtext.fontset"] = (
+    "stixsans"  #'stix'  # Or 'cm' (Computer Modern), 'stixsans', etc.
+)
+
 
 # %% Plot
 fig = plt.figure(figsize=(8, 4))
 plt.rcParams.update({"font.size": 14})
-c1 = "#F7CA0D"  # f"#2c7fb8"
+c1 = "#108883"  # f"#2c7fb8"
 c2 = "#2EBB9D"  # f"#41b6c4"
-c3 = "#108883"  # f"#a1dab4"
+c3 = "#F7CA0D"  # f"#a1dab4"
 linewidth = 3
 
 ax1 = fig.add_subplot(1, 2, 1)
@@ -111,8 +116,8 @@ for q, color in q_colors:
         color=color,
     )
 
-ax1.set_ylim([0.0, k + 0.01])
-ax1.set_xlim([0.0, theta_star + fc_minus_star])
+ax1.set_ylim([0.0, k + 0.005])
+ax1.set_xlim(var_dict["theta"]["lim"])
 ax1.set(
     xlabel=f'{theta_vardict["label"]} {theta_vardict["unit"]}',
     ylabel=f'{dtheta_vardict["label"]} {dtheta_vardict["unit"]}',
@@ -175,7 +180,7 @@ for q, color in q_colors:
 
 # theta reaches to zero earlier for q > 1
 # Hard to get the value analytically ..
-t_after_star = np.arange(4.01, 10, 1e-03)
+t_after_star = np.arange(6.5, tmax, 1e-03)
 ax2.plot(
     t_after_star,
     np.ones_like(t_after_star) * theta_w,
@@ -184,15 +189,14 @@ ax2.plot(
     color=c3,
 )
 
-
 ax2.set_ylim([0.0, theta_fc])
-ax2.set_xlim([-0.5, 7])
+ax2.set_xlim([-0.5, tmax])
 ax2.set(
     xlabel=f'{var_dict["t"]["label"]} {var_dict["t"]["unit"]}',
     ylabel=f'{theta_vardict["label"]} {theta_vardict["unit"]}',
 )
 ax2.set_title(label="B", loc="left")  # "Soil moisture drydown",
-ax2.set_xticks([t[0], 5], ["   ", "   "])
+# ax2.set_xticks([t[0], 5], ["   ", "   "])
 ax2.set_yticks(
     [theta_w, theta_star, theta_fc], [r"$\theta_{wp}$", r"$\theta^*$", r"$\theta_{fc}$"]
 )
